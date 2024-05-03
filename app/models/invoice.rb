639d9c02b090e9bcc6ac1bdf9048dd2e3fe73d8e
João Rodrigues Parra <j-r-p@outlook.com>
2024-05-03 10:57:05 -0300

    fix: deprecated 'update_attributes' to just 'update' in some controllers

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,7 +7,7 @@ class Invoice < ApplicationRecord
     paid_invoice: 2
   }
 
-  # validates :invoice_value, presence: true, numericality: { greater_than: 0 }
+  validates :invoice_value, presence: true, numericality: { greater_than: 0 }
   validates :due_date, presence: true
   validates :status, presence: true, inclusion: { in: statuses.keys }
   validates :enabled, inclusion: { in: [true, false] }
